2eece74fc62bacaaac5c4816024d7a7b119e1586
Patryk Majchrzak <pmaj12345@gmail.com>
2024-06-28 12:25:49 +0200

    adding render function

--- js/script.js
+++ js/script.js
@@ -2,7 +2,25 @@
     const tasks = [];
     const form = document.querySelector(".js-form");
     const newTaskInput = document.querySelector(".js-input");
+    const tasksListContainer = document.querySelector(".js-tasks")
     const newTask = newTaskInput.value.trim();
+
+    const render = () => {
+        if (!tasks.length) {
+            tasksListContainer.innerHTML = ""
+        }
+        else {
+            const tasksList = tasks.forEach((task) => {
+            `<ul>
+                <li>
+                    <button></button>
+                    ${task.content} 
+                    <button></button>
+                </li>
+            </ul>`
+            });
+        };
+    };
 
     const welcome = () => console.log("Hello there!");
 
