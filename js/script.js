cff9694e8630a8055e8a76f2939488e792139fef
Patryk Majchrzak <pmaj12345@gmail.com>
2024-06-28 23:54:53 +0200

    adding toggleTaskDone and RemoveTask functions activated on list button click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
+{
+    const welcome = () => console.log("Hello there!");   
+   
     const tasks = [];
+
+    const removeTask = (index) => {
+        tasks.splice(index);
+        render();
+    }
+
+    const toggleDone = (index) => {
+        tasks[index].done=!tasks[index].done;
+        render();
+    }
+
+    const listenListButtonsEvents = () => {
+        const toggleDoneButtons = document.querySelectorAll(".js-toggleDoneButton");
+
+        toggleDoneButtons.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                toggleDone(index);
+            })
+        });
+
+        const removeButtons = document.querySelectorAll(".js-removeButton");
+
+        removeButtons.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                removeTask(index);
+            })
+        });
+    };
 
     const render = () => {
         const tasksList = document.querySelector(".js-tasks");
@@ -6,14 +37,16 @@
         let newTasks = ""
 
         tasks.forEach((task) => {newTasks +=
-                `<li>
-                    <button></button>
+                `<li class="list__item ${!task.done ? "list__item--done" : ""}">
+                    <button class="list__button list__button--toggleDone js-toggleDoneButton"></button>
                         ${task.content}
-                    <button></button> 
+                    <button class="list__button list__button--remove js-removeButton"></button> 
                 </li>`
         });
 
-        tasksList.innerHTML = newTasks
+        tasksList.innerHTML = newTasks; 
+
+        listenListButtonsEvents();    
     };
 
     const addNewTask = (newTask) => {
@@ -23,7 +56,7 @@
         });
 
         render();
-    }
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -48,11 +81,10 @@
         form.addEventListener("click", onFormSubmit);
     };
 
-    const welcome = () => console.log("Hello there!");
-
     const init = () => {
         welcome();
-        listenFormSubmit();
+        listenFormSubmit();;
     };
 
     init();
+};
